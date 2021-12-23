ec644979254fc26ed7bffa66dad583e579321216
howAboutDaniel <daniel.dlugos@me.com>
2021-12-23 18:36:50 +0100

    Final commit

--- bridgeSim.m
+++ bridgeSim.m
@@ -2,6 +2,7 @@ clear; clc;
 
 %import geometry
 gm = importGeometry('Bridge_support.stl');
+
 %create model
 model = createpde('structural','modal-solid');
 model.Geometry = gm;
@@ -9,20 +10,29 @@ structuralProperties(model,'YoungsModulus',200e9, ...
                            'PoissonsRatio',0.3, ...
                            'MassDensity',7850);
 generateMesh(model,'Hmax',5);
-%
 boundries=structuralBC(model,'Face',6,'Constraint','fixed');
-%vyplotuješ 3d model
+
+%plot 3d model
 pdegplot(gm,'CellLabels','on', 'FaceLabels','on');
-%solvuješ
-RF = solve(model,'FrequencyRange',[0,100]); %*2*pi
+
+%solve
+ while 1
+    len = str2double(input('Enter the range: ','s'));
+    if len > 0 && len <= 100
+        break;
+    end
+    disp('Not a valid number. Please try again')
+ end
+
+RF = solve(model,'FrequencyRange',[0,len]); %*2*pi
 
 modeID = 1:numel(RF.NaturalFrequencies);
 tmodalResults = table(modeID.',RF.NaturalFrequencies); %/(2*pi)
 tmodalResults.Properties.VariableNames = {'Mode','Frequency'};
+cnt = length(RF.NaturalFrequencies);
 disp(tmodalResults);
 
-
-pdeplot3D(model,'ColorMapData',RF.ModeShapes.y(:,24));
+pdeplot3D(model,'ColorMapData',RF.ModeShapes.y(:,cnt));
 title(['First Mode with Frequency ', ...
-        num2str(RF.NaturalFrequencies(24)),' Hz'])
+        num2str(RF.NaturalFrequencies(1)),' Hz'])
 axis equal
